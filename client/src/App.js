972f75eac65602545d01239b8651ff1f632e1e00
krukalex <krukalex99@gmail.com>
2025-07-29 18:36:35 -0400

    troubleshooting client directory

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,3 @@
-import logo from "./logo.svg";
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import OtherPage from "./OtherPage";
@@ -9,7 +8,6 @@ function App() {
     <Router>
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Application test</h1>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
