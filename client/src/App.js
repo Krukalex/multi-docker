2ac108581c73b2af89f734795cb71fd709020a06
krukalex <krukalex99@gmail.com>
2025-07-29 18:58:59 -0400

    made small update to test deployment flow

--- client/src/App.js
+++ client/src/App.js
@@ -8,7 +8,7 @@ function App() {
     <Router>
       <div className="App">
         <header className="App-header">
-          <h1 className="App-title">Application test</h1>
+          <h1 className="App-title">Application test (updated)</h1>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
         </header>
